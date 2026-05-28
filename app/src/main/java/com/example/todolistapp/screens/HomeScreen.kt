36b709b7e22dae9d6f81d02948b0521fe3a413e4
pinky937 <mwikalis475@gmail.com>
2026-05-28 03:37:19 +0300

    Added background image

--- app/src/main/java/com/example/todolistapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/todolistapp/screens/HomeScreen.kt
@@ -1,40 +1,54 @@
 package com.example.todolistapp.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.todolistapp.R
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
 
-        Text(
-            text = "Welcome To Your To-Do App",
-            style = MaterialTheme.typography.headlineSmall
+
+        Image(
+            painter = painterResource(id = R.drawable.todo_image),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
 
-        Spacer(modifier = Modifier.height(20.dp))
 
-        Button(
-            onClick = {
-                navController.navigate("tasks")
-            },
-
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
         ) {
 
-            Text("Open Tasks")
+            Text(
+                text = "Welcome To Your To-Do App",
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    navController.navigate("tasks")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Open Tasks")
+            }
         }
     }
 }
-
